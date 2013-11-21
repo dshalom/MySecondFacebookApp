9199627ab2989fbd7e0562ffc52fd6bc834540d1
david.shalom <david.shalom@istatesystems.com>
2013-11-21 17:00:32 +0000

    first commit

---
+++ src/com/ds/mysecondfacebookapp/MainActivity.java
@@ -0,0 +1,127 @@
+package com.ds.mysecondfacebookapp;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.Menu;
+
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.UiLifecycleHelper;
+
+public class MainActivity extends FragmentActivity {
+
+	private static final int SPLASH = 0;
+	private static final int SELECTION = 1;
+	private static final int FRAGMENT_COUNT = SELECTION + 1;
+	private boolean isResumed = false;
+
+	private Fragment[] fragments = new Fragment[FRAGMENT_COUNT];
+	
+	private UiLifecycleHelper uiHelper;
+	private Session.StatusCallback callback = 
+	    new Session.StatusCallback() {
+	    @Override
+	    public void call(Session session, 
+	            SessionState state, Exception exception) {
+	        onSessionStateChange(session, state, exception);
+	    }
+	};
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		uiHelper = new UiLifecycleHelper(this, callback);
+		uiHelper.onCreate(savedInstanceState);
+		
+		FragmentManager fm = getSupportFragmentManager();
+		fragments[SPLASH] = fm.findFragmentById(R.id.splashFragment);
+		fragments[SELECTION] = fm.findFragmentById(R.id.selectionFragment);
+
+		FragmentTransaction transaction = fm.beginTransaction();
+		for (int i = 0; i < fragments.length; i++) {
+			transaction.hide(fragments[i]);
+		}
+		transaction.commit();
+	}
+
+
+	private void showFragment(int fragmentIndex, boolean addToBackStack) {
+		FragmentManager fm = getSupportFragmentManager();
+		FragmentTransaction transaction = fm.beginTransaction();
+		for (int i = 0; i < fragments.length; i++) {
+			if (i == fragmentIndex) {
+				transaction.show(fragments[i]);
+			} else {
+				transaction.hide(fragments[i]);
+			}
+		}
+		if (addToBackStack) {
+			transaction.addToBackStack(null);
+		}
+		transaction.commit();
+	}
+
+	private void onSessionStateChange(Session session, SessionState state,
+			Exception exception) {
+		// Only make changes if the activity is visible
+		if (isResumed) {
+			FragmentManager manager = getSupportFragmentManager();
+			// Get the number of entries in the back stack
+			int backStackSize = manager.getBackStackEntryCount();
+			// Clear the back stack
+			for (int i = 0; i < backStackSize; i++) {
+				manager.popBackStack();
+			}
+			if (state.isOpened()) {
+				// If the session state is open:
+				// Show the authenticated fragment
+				showFragment(SELECTION, false);
+			} else if (state.isClosed()) {
+				// If the session state is closed:
+				// Show the login fragment
+				showFragment(SPLASH, false);
+			}
+		}
+	}
+
+	@Override
+	protected void onResumeFragments() {
+		super.onResumeFragments();
+		Session session = Session.getActiveSession();
+
+		if (session != null && session.isOpened()) {
+			// if the session is already open,
+			// try to show the selection fragment
+			showFragment(SELECTION, false);
+		} else {
+			// otherwise present the splash screen
+			// and ask the person to login.
+			showFragment(SPLASH, false);
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		isResumed = true;
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+		isResumed = false;
+	}
+
+}
